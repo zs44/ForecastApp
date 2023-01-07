aba63d678e74cb465b36029eb836014a08f403bb
z-samadi <samadi83@gmail.com>
2023-01-07 21:54:38 +0330

    add forecast func

--- src/app1.js
+++ src/app1.js
@@ -1,3 +1,4 @@
+
 function formatDate(timeStamp) {
   let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday","Saturday"];
 
@@ -13,6 +14,38 @@ function formatDate(timeStamp) {
   }
   return `${day} ${hour}:${minute}`;
 }
+function displayForecast() {
+  let forecastElement = document.querySelector("#forecast");
+
+  let days = ["Thu", "Fri", "Sat", "Sun"];
+
+  let forecastHTML = `<div class="row">`;
+  days.forEach(function (day) {
+    forecastHTML =
+      forecastHTML +
+      `
+      <div class="col-2">
+        <div class="weather-forecast-date">${day}</div>
+        <img
+          src="http://openweathermap.org/img/wn/50d@2x.png"
+          alt=""
+          width="42"
+        />
+        <div class="weather-forecast-temperatures">
+          <span class="weather-forecast-temperature-max"> 18° </span>
+          <span class="weather-forecast-temperature-min"> 12° </span>
+        </div>
+      </div>
+  `;
+  });
+
+  forecastHTML = forecastHTML + `</div>`;
+  forecastElement.innerHTML = forecastHTML;
+  console.log(forecastHTML);
+}
+ 
+
+displayForecast();
 function displayTemp(response) {
   let cityElm = document.querySelector("#city");
   cityElm.innerHTML = `${response.data.name} Province`;
